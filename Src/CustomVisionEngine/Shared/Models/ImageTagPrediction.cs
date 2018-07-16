26ff71884c0cabd78128b4c19128d161ba5ae75d
Marco Minerva <marco.minerva@gmail.com>
2018-07-16 10:50:40 +0200

    Update to Custom Vision API 2.0. Better support for UWP

--- Src/CustomVisionEngine/Shared/Models/ImageTagPrediction.cs
+++ Src/CustomVisionEngine/Shared/Models/ImageTagPrediction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace Plugin.CustomVisionEngine.Models
     {
         public Guid TagId { get; set; }
 
+        [JsonProperty("TagName")]
         public string Tag { get; set; }
 
         public double Probability { get; set; }
